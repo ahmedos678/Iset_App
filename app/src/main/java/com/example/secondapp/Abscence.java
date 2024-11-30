be5ecc7e3988d432655dd1be5d8a79a137ff524a
ahmedos678 <ahmedslimeni404@gmail.com>
2024-11-30 21:36:48 +0100

    third commit

--- app/src/main/java/com/example/secondapp/Abscence.java
+++ app/src/main/java/com/example/secondapp/Abscence.java
@@ -2,8 +2,6 @@ package com.example.secondapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,21 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignUp extends AppCompatActivity {
+public class Abscence extends AppCompatActivity {
 
-    private Button btnsignup;
-    private TextView signin;
+    private TextView semestre1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_abscence);
 
-        signin=findViewById(R.id.signin_signup);
-
-        signin.setOnClickListener(view -> {
-            startActivity(new Intent(this,SignIn.class));
+        semestre1=findViewById(R.id.taux);
+        semestre1.setOnClickListener(view -> {
+            startActivity(new Intent(this,Semestre1.class));
         });
     }
 }
--- app/src/main/java/com/example/secondapp/SignIn.java
+++ app/src/main/java/com/example/secondapp/SignIn.java
@@ -1,6 +1,9 @@
 package com.example.secondapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignIn extends AppCompatActivity {
 
+    private TextView signUp;
+    private Button btnSign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        signUp = findViewById(R.id.btn_signup_signin);
+        btnSign = findViewById(R.id.btn_signin_signin);
+
+
+        signUp.setOnClickListener(view -> {
+            startActivity(new Intent(this,SignUp.class));
+        });
+
+        btnSign.setOnClickListener(view -> {
+            startActivity(new Intent(this,MainActivity.class));
         });
     }
+
+
 }
